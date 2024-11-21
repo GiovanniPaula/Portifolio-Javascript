b059694f7707a7c2c95c9274ee16e126db7cde21
Giovanni De Paula <giovannipaula319@gmail.com>
2024-11-21 16:09:32 -0300

    Primeiro commit: adicionando meu portfólio

---
+++ PORTIFOLIO3/script.js
@@ -0,0 +1,7 @@
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  themeToggle.textContent = 
+    document.body.classList.contains('dark-mode') ? 'Modo Claro' : 'Modo Escuro';
+});
